b49bbbc396c4f4a331552f884f3bb6938cbe51cb
Yandi <yandikurniawan@gmail.com>
2019-03-18 13:34:39 +0700

    added account list

---
+++ components/AccountsList.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		this is account list
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'account-list'
+}
+</script>
---
+++ pages/account.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<AccountsList />
+	</div>
+</template>
+
+<script>
+import AccountsList from '~/components/AccountsList.vue'
+
+export default {
+	component: {
+		AccountsList
+	}
+}
+</script>
